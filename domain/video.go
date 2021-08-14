3bfbba4b5a69521981e4aff9f0c9dde2eba13dbe
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-08-13 21:02:30 -0300

    feat: adicionados iniciador, construtor e validador de video.go

--- domain/video.go
+++ domain/video.go
@@ -1,10 +1,27 @@
 package domain
 
-import "time"
+import (
+	"github.com/asaskevich/govalidator"
+	"time"
+)
 
 type Video struct {
-	ID         string
-	ResourceID string
-	FilePath   string
-	CreatedAt  time.Time
+	ID         string    `valid:"uuid"`
+	ResourceID string    `valid:"notnull"`
+	FilePath   string    `valid:"notnull"`
+	CreatedAt  time.Time `valid:"-"`
+}
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+func NewVideo() *Video {
+	return &Video{}
+}
+
+func (video *Video) Validate() error {
+	_, err := govalidator.ValidateStruct(video)
+
+	return err
 }
