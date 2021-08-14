45c3afaa0154c685fb6f55e82eeec14b01207bcb
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2021-08-13 21:31:11 -0300

    feat: criada implementacao de database com gorm

---
+++ framework/database/db.go
@@ -0,0 +1,63 @@
+package database
+
+import (
+	"github.com/jeanmolossi/codeflix-microservice-videos-encoder/domain"
+	"github.com/jinzhu/gorm"
+	"log"
+)
+
+type Database struct {
+	Db            *gorm.DB
+	Dsn           string
+	DsnTest       string
+	DbType        string
+	DbTypeTest    string
+	Debug         bool
+	AutoMigrateDb bool
+	Env           string
+}
+
+func NewDb() *Database {
+	return &Database{}
+}
+
+func NewDbTest() *gorm.DB {
+	dbInstance := NewDb()
+	dbInstance.Env = "test"
+	dbInstance.DbTypeTest = "sqlite3"
+	dbInstance.DsnTest = ":memory:"
+	dbInstance.AutoMigrateDb = true
+	dbInstance.Debug = true
+
+	connection, err := dbInstance.Connect()
+
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
+
+	return connection
+}
+
+func (db *Database) Connect() (*gorm.DB, error) {
+	var err error
+
+	if db.Env != "test" {
+		db.Db, err = gorm.Open(db.DbType, db.Dsn)
+	} else {
+		db.Db, err = gorm.Open(db.DbTypeTest, db.DsnTest)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if db.Debug {
+		db.Db.LogMode(true)
+	}
+
+	if db.AutoMigrateDb {
+		db.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+	}
+
+	return db.Db, nil
+}
